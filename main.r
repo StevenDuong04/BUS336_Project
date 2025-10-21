863d82f960ea9733efa47d114a6aae4617072517
Steven Duong <151969699+StevenDuong04@users.noreply.github.com>
2025-10-20 18:13:14 -0700

    Added new data files. Tidied the main R code with comments to show what we need to do to prepare for the Tableau section

--- main.r
+++ main.r
@@ -1,4 +1,49 @@
-# Steven Duong
-# Darian Sidhu
-# Brady Van Unen
-# BUS336 Project
+# ==========================================================
+# Project: BUS336 Hackathon
+# Purpose: Data cleaning, exploration, and preparation for Tableau
+# Author: Steven Duong, Darian Sidhu, Brady Van Unen
+# Date: Oct 20, 2025
+# ==========================================================
+
+# ---------------------------
+# 1. Load libraries
+# ---------------------------
+library(tidyverse)
+library(lubridate)
+
+
+# ---------------------------
+# 2. Load Data
+# ---------------------------
+
+
+
+# ---------------------------
+# 3. Inspect data
+# ---------------------------
+
+
+
+# ---------------------------
+# 4. Clean Data
+# ---------------------------
+
+
+
+# ---------------------------
+# 5. Feature engineering
+# ---------------------------
+
+
+
+# ---------------------------
+# 6. EDA - Exploratory Data Analysis (IF needed)
+# ---------------------------
+
+
+
+# ---------------------------
+# 7. Export cleaned data file for Tableau
+# ---------------------------
+
+
