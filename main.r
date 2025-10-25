ddf6ca1bae189df95a5a3989af921348aa74db83
Steven <vduong.steven@gmail.com>
2025-10-25 16:08:42 -0700

    Added two main datasets and inspected the datasets

--- main.r
+++ main.r
@@ -2,7 +2,7 @@
 # Project: BUS336 Hackathon
 # Purpose: Data cleaning, exploration, and preparation for Tableau
 # Author: Steven Duong, Darian Sidhu, Brady Van Unen
-# Date: Oct 20, 2025
+# Date: Oct 25, 2025
 # ==========================================================
 
 # ---------------------------
@@ -10,19 +10,33 @@
 # ---------------------------
 library(tidyverse)
 library(lubridate)
+library(ggplot2)
+library(GGally)
+library(caTools)
+library(MASS) # If using select make sure to specify from dplyr by using dplyr::select()
+library(readxl)
 
 
 # ---------------------------
 # 2. Load Data
 # ---------------------------
+# Use excel_sheets("2024 Bioretention Condition Assessment_City Studio Data.xlsx") to see existing sheets
+# Seperate sheets into different data frames (currently as a tibble)
 
+BCA2024 <- read_excel("2024 Bioretention Condition Assessment_City Studio Data.xlsx", sheet = "24CSTable")
+BCA2022 <- read_excel("2024 Bioretention Condition Assessment_City Studio Data.xlsx", sheet = "22CSTable")
 
 
 # ---------------------------
 # 3. Inspect data
 # ---------------------------
+glimpse(BCA2024)
+summary(BCA2024)
+head(BCA2024)
 
-
+glimpse(BCA2022)
+summary(BCA2022)
+head(BCA2022)
 
 # ---------------------------
 # 4. Clean Data
@@ -37,7 +51,7 @@ library(lubridate)
 
 
 # ---------------------------
-# 6. EDA - Exploratory Data Analysis (IF needed)
+# 6. EDA - Exploratory Data Analysis
 # ---------------------------
 
 
